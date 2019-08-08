e4712a16d9e8eaa14a04e022de3e19d5ac5bb3fe
Bhupinder Singh <khanujabhupinder09@gmail.com>
2019-08-09 00:09:34 +0530

    Updated Distribute Apple Code

--- August Challenge 2019 (Division 2)/Distribute_Apples.py
+++ August Challenge 2019 (Division 2)/Distribute_Apples.py
@@ -1,7 +1,13 @@
 test_cases = int(input())
 for i in range(test_cases):
   y = [int(i) for i in input().split()]
-  if y[1] <= 2:
+  if y[1] == 1:
     print("NO")
-  else:
+  elif y[0] == y[1]:
     print("YES")
+  else:
+    square = y[1]*y[1]
+    if y[0]%square==0:
+      print("NO")
+    else:
+      print("YES")
