f77ad751d495b0de46134fca3308275ef42172d7
Bhupinder Singh <khanujabhupinder09@gmail.com>
2020-05-12 09:12:09 +0530

    Updated Codes | Completed Greedy Week 2

--- Coursera Algorithmic Toolbox/Week 2/fibonacci_number_again.py
+++ Coursera Algorithmic Toolbox/Week 2/fibonacci_number_again.py
@@ -8,6 +8,8 @@ def pisano(n):
 def last_digit(f, n):
   pissano = pisano(n)
   f = f%pissano
+  if(f<=1):
+    return f
   pre, cur = 0, 1
   for i in range(f-1):
     pre, cur = cur, pre+cur
--- Coursera Algorithmic Toolbox/Week 3/maximum_salary.py
+++ Coursera Algorithmic Toolbox/Week 3/maximum_salary.py
@@ -1,7 +1,18 @@
-def salary(a):
+def IsGreaterOrEqual(digit, max_digit):
+  return int(str(digit)+str(max_digit))>=int(str(max_digit)+str(digit))
   
 
+def salary(n, a):
+  ans = []
+  while a!=[]:
+    max_digit = 0
+    for digit in a:
+      if IsGreaterOrEqual(digit, max_digit):
+        max_digit = digit
+    ans.append(max_digit)
+    a.remove(max_digit)
+  return ans
 
 n = int(input())
 a = list(map(int, input().split()))
-print(salary(a))
+print(''.join([str(i) for i in salary(n, a)]))
